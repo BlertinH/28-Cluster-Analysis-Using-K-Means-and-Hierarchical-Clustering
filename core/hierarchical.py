d9a7f7c220a2deb04d3e20308d84edee55948c5e
ElisaGjurkaj <elisa.gjrk@gmail.com>
2025-12-06 22:50:44 +0100

    Add initial hierarchical clustering function

--- core/hierarchical.py
+++ core/hierarchical.py
@@ -1,1 +1,12 @@
-# ...
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
+import matplotlib.pyplot as plt
+
+def hierarchical_clustering(data, num_clusters):
+    Z = linkage(data, method="ward", metric="euclidean")
+
+    fig = plt.Figure(figsize=(7, 4))
+    ax = fig.add_subplot(111)
+    dendrogram(Z, ax=ax)
+
+    labels = fcluster(Z, num_clusters, criterion="maxclust")
+    return labels, fig
