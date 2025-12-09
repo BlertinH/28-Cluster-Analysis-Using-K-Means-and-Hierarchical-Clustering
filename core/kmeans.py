5cf26a6e50c168a21093ce3e037911cfc65d4c59
BlertinH <blertin.hamza@student.uni-pr.edu>
2025-12-09 17:37:17 +0100

    added adding _closest_centroid helper in kmeans

--- core/kmeans.py
+++ core/kmeans.py
@@ -5,27 +5,30 @@ class KMeans:
         self.k = k
         self.max_iters = max_iters
 
+    def _closest_centroid(self, X):
+        labels = []
+        for p in X:
+            dists = np.linalg.norm(p - self.centroids, axis=1)
+            labels.append(np.argmin(dists))
+        return np.array(labels)
+
     def fit(self, X):
         self.centroids = X[np.random.choice(len(X), self.k, replace=False)]
 
         for _ in range(self.max_iters):
-            labels = []
-            for p in X:
-                d = np.linalg.norm(p - self.centroids, axis=1)
-                labels.append(np.argmin(d))
-            labels = np.array(labels)
+            labels = self._closest_centroid(X)
 
             new_centroids = []
             for i in range(self.k):
                 pts = X[labels == i]
-                if len(pts) > 0:
-                    new_centroids.append(pts.mean(axis=0))
-                else:
-                    new_centroids.append(self.centroids[i])
+                new_centroids.append(pts.mean(axis=0) if len(pts) > 0 else self.centroids[i])
 
             new_centroids = np.array(new_centroids)
+
             if np.allclose(new_centroids, self.centroids):
                 break
             self.centroids = new_centroids
 
         return labels
+
+
