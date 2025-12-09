b68033beaf8a300543cbb76da22e050bb49315fa
ElisaGjurkaj <elisa.gjrk@gmail.com>
2025-12-09 17:29:43 +0100

    Add hierarchical clustering function with metric selection

--- core/hierarchical.py
+++ core/hierarchical.py
@@ -1,12 +1,23 @@
 from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 import matplotlib.pyplot as plt
 
-def hierarchical_clustering(data, num_clusters):
-    Z = linkage(data, method="ward", metric="euclidean")
+def hierarchical_clustering(data,metric,num_clusters):
+    if metric =="cityblock":
+        method ="complete"
+        metric_for_linkage="cityblock"
+    else:
+        method = "ward"
+        metric_for_linkage = "euclidean"
+
+    Z = linkage(data, method=method, metric=metric_for_linkage)
 
     fig = plt.Figure(figsize=(7, 4))
     ax = fig.add_subplot(111)
     dendrogram(Z, ax=ax)
+
+    ax.set_title("Hierarchical Dendogram(Ward)")
+    ax.set_xlabel("Sample Index")
+    ax.set_ylabel("Distance")
 
     labels = fcluster(Z, num_clusters, criterion="maxclust")
     return labels, fig
